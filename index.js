5682472826d4f20ddaf87e7365ea121d74fabd41
Lukas <77848587+lukascobit@users.noreply.github.com>
2021-03-02 16:12:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function RandomCreator(){
+    
+    let RandomNumber = Math.random() * 10
+    let rr = Math.round(RandomNumber)
+
+
+    let TextRan = document.getElementById("NT").innerHTML = rr
+
+   
+    
+    
+
+
+    
+}
